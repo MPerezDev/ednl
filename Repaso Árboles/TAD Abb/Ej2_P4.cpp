17adb91f58983bef11156f37d098e65fa116c806
[manupruiz] <[rayerito23@gmail.com]>
2022-04-25 17:41:30 +0200

    p4

---
+++ Repaso Árboles/TAD Abb/Ej2_P4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include "abb.h"
+
+using namespace std;
+
+
+template <typename T>
+void equilibrar(Abb<T>& A){
+
+    if(!A.vacio()){
+
+        vector<T> v;
+
+        
+
+    }
+
+
+}
